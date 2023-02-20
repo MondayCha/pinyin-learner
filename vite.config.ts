6d2bb106aeb1543d050146164bb84f5217066ede
13718250032 <18373580@buaa.edu.cn>
2023-02-20 11:06:23 +0800

    chore: feat: integrate tauri into pinyin

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import vitApp from '@vitjs/vit'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pinyin/',
+  base: '/',
   plugins: [
     react(),
     tsconfigPaths(),
@@ -32,7 +32,7 @@ export default defineConfig({
     }),
   ],
   server: {
-    open: true,
+    strictPort: true,
     port: 8000,
   },
   resolve: {
